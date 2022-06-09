d6a4b4503a4cf599e833fed572e55ad732a6ef06
RobStenning <46462192+RobStenning@users.noreply.github.com>
2022-06-09 22:13:48 +0100

    error handling and page added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/dog-and-bone');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./tools/catchAsync');
+const ExpressError = require('./tools/ExpressError');
 const methodOverride = require('method-override');
 const Beer = require('./models/beers');
 const beers = require('./models/beers');
@@ -29,55 +31,66 @@ app.get('/', async (req, res) => {
     res.render('home', { beers, displayOntap });
 })
 
-app.get('/taplist', async (req, res) => {
+app.get('/taplist', catchAsync(async (req, res, next) => {
     const beers = await Beer.find({})
     const displayOntap = await Beer.findOne({ ontap: 'pouring'});
     const displayIncoming = await Beer.findOne({ ontap: 'kegged'});
     const displayBottled = await Beer.findOne({ ontap: 'bottled'});
     const displayPrevious = await Beer.findOne({ ontap: 'previous'});
     res.render('beers/taplist', { beers, displayOntap, displayIncoming, displayBottled, displayPrevious });
-})
+}))
 
-app.get('/previousbeers', async (req, res) => {
+app.get('/previousbeers', catchAsync(async (req, res, next) => {
     const beers = await Beer.find({})
     const displayPrevious = await Beer.findOne({ ontap: 'previous'});
     res.render('beers/previousbeers', { beers, displayPrevious});
-})
+}))
 
 app.get('/new', (req, res) => {
     res.render('beers/new');
 })
 
-app.post('/beers', async (req, res) => {
+app.post('/beers', catchAsync(async (req, res, next) => { 
+    if(!req.body.campground) throw new ExpressError('Invalid Data', 400);
     const beer = new Beer(req.body.beer)
     await beer.save();
     res.redirect(`/beers/${beer._id}`)
-})
+}))
 
-app.get('/beers/:id', async (req, res) => {
+app.get('/beers/:id', catchAsync(async (req, res, next) => {
     const beer = await Beer.findById(req.params.id)
     res.render('beers/info', { beer })
-})
+}))
 
-app.get('/beers/:id/edit', async (req, res) => {
+app.get('/beers/:id/edit', catchAsync(async (req, res, next) => {
     const beer = await Beer.findById(req.params.id)
     res.render('beers/edit', { beer })
-})
+}))
 
-app.put('/beers/:id', async (req, res) => {
+app.put('/beers/:id', catchAsync( async (req, res, next) => {
     const { id } = req.params
     const beer = await beers.findByIdAndUpdate(id, { ...req.body.beer })
     res.redirect(`/beers/${beer._id}`)
-})
+}))
 
-app.delete('/beers/:id', async (req, res) => {
+app.delete('/beers/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await beers.findByIdAndDelete(id)
     res.redirect('/taplist')
-})
+}))
 
 app.get('/login', (req, res) =>{
     res.render('login');
+})
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message =  'Something Went Wrong' 
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
